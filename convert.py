2245912c2cc6bccdc3b91418b31ad879660f9bde
Michael Van Delft <michael@bluelight.local>
2013-07-08 19:50:20 +0800

    Committer: Michael Van Delft <michael@bluelight.local> 	modified:   convert.py

--- convert.py
+++ convert.py
@@ -7,14 +7,23 @@
 #
 
 import optparse
+supportedLanguages = ['en-US', 'en-GB', 'de-DE', 'es-ES', 'fr-FR', 'it-IT']
 
 def validate_options(options, args):
     """
     Given a set of command line options, performs various validation checks
     """
 
-    if len(args) > 1:
+    if len(args) > 0:
         parser.error("Too many arguments.")
+
+    if options.language not in supportedLanguages:
+        print "Language" + options.language + "is currently not available.\n\
+        'Available languages are ' + ', '.join(languages[:-1]) + \
+        ' and ' + languages[-1] + '.\n'""
+
+
+
 
 
 if __name__ == '__main__':
@@ -22,15 +31,28 @@ if __name__ == '__main__':
     usage = "usage: %prog [options]"
     parser = optparse.OptionParser(usage)
 
-    parser.add_option("-i", "--infile", dest="inFile",
-                      default='./test.pdf',
-                      help="The file to be converted")
-
-    parser.add_option("-o", "--outfile", dest="outFile",
-                      default='./test.ogg',
-                      help="The location to output to")
+    parser.add_option("-i", "--input", dest="inFile",
+                      default="./test.pdf",
+                      help="The PDF file to be converted:")
+    parser.add_option("-o", "--output", dest="outFile",
+                      default="./test.ogg",
+                      help="Output to the specified file: (ogg format)")
+    parser.add_option("-l", "--language", dest="language",
+                      default="en-US",
+                      help="Language to speak: (default is en-US) avalible \
+                      languages inclue 'en-US', 'en-GB', 'de-DE', 'es-ES', \
+                      'fr-FR', 'it-IT'")
+    parser.add_option("-v", "--volume", dest="volume",
+                      default=1.0, type="float",
+                      help="Output volume: (default is 1.0)")
+    parser.add_option("-r", "--rate", dest="rate",
+                      default=0, type="int",
+                      help="Rate of speech from -90 to 9900: (default is 0)")
+    parser.add_option("-p", "--pitch", dest="pitch",
+                      default=0, type="int",
+                      help="Voice pitch from -79 to 39: (default is 0)")
 
     (options, args) = parser.parse_args()
     validate_options(options, args)
 
-    print(options.inFile)
+    print(options.volume)
